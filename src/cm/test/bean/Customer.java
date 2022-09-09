0b6086b5bc068b5164086beb520221cd425a6f17
GT <hello@GT.cn>
2022-09-09 14:17:19 +0800

    Initial commit

---
+++ src/cm/test/bean/Customer.java
@@ -0,0 +1,5 @@
+package cm.test.bean;
+//客户角色
+public class Customer extends  User{
+    //
+}
---
+++ src/cm/test/run/A.java
@@ -0,0 +1,8 @@
+package cm.test.run;
+
+public class A {
+    public static void main(String[] args) {
+        String a = null;
+        System.err.println(a.split(","));
+    }
+}
